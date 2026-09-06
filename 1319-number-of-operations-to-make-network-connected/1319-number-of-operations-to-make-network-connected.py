563bfbc9168103df63e76baf1672a98c5dd45b7b
Manvitha Reddy Adulla <manvithasiri282006@gmail.com>
2026-09-06 22:09:01 +0530

    Time: 33 ms (70.94%), Space: 41.5 MB (18.58%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
@@ -1,36 +1,29 @@
-from typing  import List
 class Solution:
-    class disjointset:
-        def __init__(self,n):
-            self.parent = [i for i in range(n)]
-            self.rank = [0]*n
-        def find(self,u,v):
-            return self.findp(u) == self.findp(v)
-        def findp(self,u):
-            if self.parent[u] != u :
-                self.parent[u] = self.findp(self.parent[u])
-            return self.parent[u] 
-        def unionbyrank(self,u,v):
-            pu = self.findp(u)
-            pv = self.findp(v)
-
-            if pu == pv:
-                return
-
-            if self.rank[pu] < self.rank[pv]:
-                self.parent[pu] = pv
-            elif self.rank[pv] < self.rank[pu]:
-                self.parent[pv] = pu
-            else:
-                self.parent[pv] = pu
-                self.rank[pu] += 1
-
-
     def makeConnected(self, n: int, connections: List[List[int]]) -> int:
-        if len(connections) < n-1 :
+
+        if len(connections) < n - 1:
             return -1
-        ds = self.disjointset(n)
-        for u,v in connections:
-            ds.unionbyrank(u,v)
-        compo = sum(1 for i in range(n) if ds.findp(i) == i )
-        return compo - 1
+
+        graph = [[] for _ in range(n)]
+
+        for u, v in connections:
+            graph[u].append(v)
+            graph[v].append(u)
+
+        visited = [False] * n
+
+        def dfs(node):
+            visited[node] = True
+
+            for nei in graph[node]:
+                if not visited[nei]:
+                    dfs(nei)
+
+        components = 0
+
+        for i in range(n):
+            if not visited[i]:
+                components += 1
+                dfs(i)
+
+        return components - 1
