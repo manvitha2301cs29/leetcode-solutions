bed13bfcefd21c6a7de6b8eb4a9804eefe630d94
Manvitha Reddy Adulla <manvithasiri282006@gmail.com>
2026-09-07 21:48:37 +0530

    Time: 659 ms (19.69%), Space: 20.1 MB (60.68%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.py
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.py
@@ -5,18 +5,35 @@ class Solution:
         # we can form a group if it has same x or  y 
         # ans = n - no of groups 
         # we can find no of connected components with dfs 
-        vis = set()
-        compo = 0 
-        def dfs(i):
-            if i in vis or i > n-1:
+        rank = [0]*n 
+        par = [i for i in range(n)]
+        def findp(u):
+            if par[u] != u :
+                par[u] = findp(par[u])
+            return par[u]
+        def find(u,v):
+            return findp(u) == findp(v)
+        def union(u,v):
+            pu = findp(u)
+            pv = findp(v)
+            if pu == pv :
                 return 
-            vis.add(i)
-            for j in range(n):
-                if j not in vis :
-                    if stones[i][0] == stones[j][0] or stones[i][1] == stones[j][1] :
-                        dfs(j)
+            if rank[pu] < rank[pv]:
+                par[pu]  = pv 
+            elif rank[pu] > rank[pv]:
+                par[pv]  = pu
+            else :
+                par[pv]  = pu
+                rank[pu] += 1 
         for i in range(n):
-            if i not in vis :
-                compo += 1 
-                dfs(i)
-        return n - compo 
+            for j in range(i+1,n):
+                if stones[i][0] == stones[j][0] or stones[i][1] == stones[j][1]:
+                    union(i,j)
+        # no of roots = no of connected components 
+        nr = 0 
+        for i in range(n):
+            if i == findp(i) :
+                nr += 1 
+        return n - nr 
+
+            
